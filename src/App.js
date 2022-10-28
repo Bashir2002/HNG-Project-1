e869bbfa68b1cadfe002e278c200f7d80616f701
Headmaster <aliubashirtaiye2002@gmail.com>
2022-10-28 16:34:29 +0100

    Main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import slack from './slack.png'
+import Github from './github.png'
+import logo from './I4G.png'
+import zuri from './zuri.png'
+import './App.css'
+import data from './data.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App'>
+      <img src='https://github.com/Bashir2002.png' alt='' />
+      <h3>Aliu Bashir</h3>
+      <div className='links'>
+        {data.map((item) => {
+          const { id, key, url, sit, desc } = item
+
+          return (
+            <button key={key} id={id} class={sit}>
+              <a href={url} target='_blank'>
+                {desc}{' '}
+              </a>
+            </button>
+          )
+        })}
+      </div>
+      <div className='socials'>
+        <a href=''>
+          <img src={slack} alt='' />
         </a>
-      </header>
+        <a href='https://github.com/Bashir2002'>
+          <img src={Github} alt='' />
+        </a>
+      </div>
+      <section className='footers'>
+        <img src={zuri} alt='' />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={logo} alt='' />
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    key: 1,
+    id: 'twitter',
+    url: 'http://twitter.com/danbashi_1',
+    sit: '',
+    desc: 'Twitter Link',
+  },
+  {
+    key: 2,
+    id: 'slack',
+    sit: 'none',
+    url: 'http://twitter.com/danbashi_1',
+    desc: 'Slack',
+  },
+  {
+    key: 4,
+    id: 'btn__zuri',
+    sit: '',
+    url: 'https://training.zuri.team/',
+    desc: 'Zuri Team',
+  },
+  {
+    key: 5,
+    id: 'books',
+    sit: '',
+    url: 'https://books.zuri.team/',
+    desc: 'Zuri Books',
+  },
+  {
+    key: 6,
+    id: 'book__python',
+    sit: '',
+    url: 'https://books.zuri.team/python-for-beginners?ref_id=AliuBashir',
+    desc: 'Python Books',
+  },
+  {
+    key: 7,
+    id: 'pitch',
+    sit: '',
+    url: ' https://background.zuri.team',
+    desc: 'Background Check for Coders',
+  },
+  {
+    key: 8,
+    id: 'book__design',
+    sit: '',
+    url: 'https://books.zuri.team/design-rules',
+    desc: 'Design Books',
+  },
+]
+export default data
